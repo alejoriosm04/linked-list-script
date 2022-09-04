14cdbe8769e116ea74389d86002226ce53b8f756
Luis M. Torres-Villegas <111613017+LuisForPresident@users.noreply.github.com>
2022-09-04 11:01:08 -0500

    Style: make comments conciser

--- template.py
+++ template.py
@@ -1,17 +1,17 @@
-# Use this always, do not modify this
+# DO NOT MODIFY THIS
 from linkedLists import *
 
 
-# Write here the function that you want to test with Linked Lists
+# Function to test goes here
 
 
 
-# In the main function, create the Linked List and invoke the function according it is behavior
-# Check the documentation for more details
+# Create the Linked List here and invoke the function
+# Check the README for more details
 def main():
     pass
 
 
-# Use this always, this will allow the main function to work correctly, and is a good practice to use this notation for invoke the main function in Python
+# Use this always. Allows main function to work correctly
 if __name__ == '__main__':
     main()
